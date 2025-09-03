9a86821e3f692731f7e033813b2f171c0f93f13b
Breno-Lima <breno.souza@academico.ufpb.br>
2025-09-03 14:55:55 -0300

    fix

--- constants/maintenance.ts
+++ constants/maintenance.ts
@@ -1,7 +1,7 @@
 export const MAINTENANCE_CONFIG = {
   // Controla se o site está em manutenção
   // Mude para true para ativar o modo de manutenção
-  isMaintenanceMode: true,
+  isMaintenanceMode: false,
   
   // Mensagem exibida na página de manutenção
   maintenanceMessage: "Estamos trabalhando para melhorar sua experiência",
